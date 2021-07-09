281d7697e0da09b3921b300743180378f767d36b
rewdaraw <rewdaraw@gmail.com>
2021-07-09 11:14:43 +0900

    apollo-server basic setting

---
+++ fetchData.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+        id: 1,
+        name: "user1",
+        age: "19",
+        married: false
+    },
+    {
+        id: 2,
+        name: "user2",
+        age: "24",
+        married: true
+    },
+    {
+        id: 3,
+        name: "user3",
+        age: "36",
+        married: false
+    }
+]
+
+module.exports = {users}
